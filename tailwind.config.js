e981ea0b7ced7e9dd575d9bc4d7a90c894127b26
David Di Biase <daviddibiase@dbase-macbook.local>
2020-12-22 10:56:49 -0500

    Minor clean-up of packages and font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 
+const isProd = process.env["npm_lifecycle_event"] === "build";
 
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       container: {
         padding: '5rem',
       },
       colors: {
-        // This color has accessibility issues
         primary: "#4483c1",
         solid: {
           DEFAULT: '#2c4f7c',
@@ -18,11 +17,35 @@ module.exports = {
           light: '#4f88c6'
         }
       },
-      fontFamily: {},
+      fontFamily: {
+        display: [
+          // "Gordita",
+          " ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
     },
   },
   variants: {},
   plugins: [
     require('@tailwindcss/typography')
   ],
+  purge: {
+    enabled: isProd,
+    mode: "layers",
+    layers: ["base", "components", "utilities"],
+    content: ["src/**/*.html", "src/**/*.tsx"],
+  },
 }
