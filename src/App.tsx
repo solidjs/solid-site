993ea4b1df93dd118e8d9c0f975001cf212b0012
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-25 21:43:34 +0100

    Remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,6 @@ import { Route } from 'solid-app-router';
 
 import Footer from './components/Footer';
 
-import { Parser, HtmlRenderer } from 'commonmark';
-
-// TODO: Remove move, this is just a proof of concept to demonstrate common mark working
-const reader = new Parser();
-const writer = new HtmlRenderer();
-const parsed = reader.parse('Hello *world*');
-
-const result = writer.render(parsed);
-
-console.log({ result });
-// end of demo
-
 export const App: Component = () => (
   <>
     <Route />
