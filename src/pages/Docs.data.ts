82b1b6130560d2ae7a7cd7e3bafa06efde8f4b6a
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2020-12-27 00:41:33 +0100

    Fix Doc page

--- src/pages/Docs.data.ts
+++ src/pages/Docs.data.ts
@@ -1,15 +1,21 @@
 import { RouteDefinition } from 'solid-app-router';
-import { createComputed, createResource } from 'solid-js';
+import { createComputed, createResource, createSignal } from 'solid-js';
 
 const fetchMarkdown = (version: string, id: string) => () =>
-  fetch(`/docs/md/${version}/${id}.md`).then((r) => r.text());
+  fetch(`/docs/md/${version}/${id}.md`).then((r) => {
+    // This is probably not the best place to do this
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    return r.text();
+  });
 
 export const DocsData: RouteDefinition['data'] = (props) => {
   const [markdown, loadMarkdown] = createResource<string>();
   let previousVersion: string;
   let previousPage: string;
+  const [hash, setHash] = createSignal(location.hash);
 
   createComputed(() => {
+    setHash(location.hash);
     if (previousPage === props.params.page && previousVersion === props.params.version) return;
 
     void loadMarkdown(fetchMarkdown(props.params.version as string, props.params.page as string));
@@ -30,6 +36,9 @@ export const DocsData: RouteDefinition['data'] = (props) => {
     },
     get params() {
       return props.params;
+    },
+    get hash() {
+      return hash();
     },
   };
 };
--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -32,6 +32,7 @@ const Docs: Component<{
   loading: boolean;
   version: string;
   params: Record<string, string>;
+  hash: string;
 }> = (props) => {
   const [sections, setSections] = createSignal<{ id: string; title: string }[]>([]);
   const current_docs = createMemo(() => {
@@ -49,12 +50,16 @@ const Docs: Component<{
       <Header title="Documentation" />
       <div class="container grid my-10 grid-cols-12 gap-4">
         <div class="col-span-3">
-          <div class="py-5 sticky" style={{ top: '8rem' }}>
+          <div
+            class="py-5 sticky flex flex-col"
+            style={{ top: '8rem', height: 'calc(100vh - 80px - 2.5rem)' }}
+          >
             <div class="border rounded-md text-md p-3 mb-4">Version {current_docs().version}</div>
-            {current_docs().files.map((file) => (
+
+            <ul class="overflow-auto flex-1">
               <For each={current_docs().files}>
                 {(file) => (
-                  <>
+                  <li>
                     <Link
                       class="block uppercase text-solid-medium border-b px-2 transition pb-3 text-sm my-4 hover:text-gray-400"
                       href={`/docs/${current_docs().version}/${file}`}
@@ -65,17 +70,21 @@ const Docs: Component<{
                       <For each={sections()}>
                         {(section) => (
                           <a
-                            class="block px-5 border-b border-gray-100 pb-3 text-sm my-4 hover:text-gray-400"
+                            class="block px-5 border-b border-gray-100 pb-3 text-sm my-4 break-words"
+                            classList={{
+                              'text-solid hover:text-solid-dark': `#${section.id}` === props.hash,
+                              'hover:text-gray-400': `#${section.id}` !== props.hash,
+                            }}
                             href={`#${section.id}`}
                             children={section.title}
                           />
                         )}
                       </For>
                     </Show>
-                  </>
+                  </li>
                 )}
               </For>
-            ))}
+            </ul>
           </div>
         </div>
         <div class="col-span-9 p-5">
