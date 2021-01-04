f413407267ce7b1f180311e873530a62779a8c2a
David Di Biase <daviddibiase@dbase-macbook.local>
2021-01-04 17:08:49 -0500

    Added Solid Meta to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import type { Component } from 'solid-js';
 import { Route } from 'solid-app-router';
+import { MetaProvider, Link, Meta } from 'solid-meta';
 
 import Footer from './components/Footer';
 
 export const App: Component = () => (
-  <>
+  <MetaProvider>
+    <Link rel="canonical" href="https://www.solidjs.com/" />
+    <Meta name="keywords" content="javascript solid ui framework" />
+    <Meta
+      name="description"
+      content="The next generation fine-grained reactive Javascript UI library."
+    />
     <main class="min-h-screen">
       <Route />
     </main>
-
     <Footer />
-  </>
+  </MetaProvider>
 );
