23cd9ff19ea458457bceb7a167074747f88aaa92
Alexandre <amoutonbrady@gmail.com>
2021-05-14 13:49:11 +0200

    Cleanup some styles on the tutorial page + forcing vertical layout on the playground

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,6 +1,9 @@
 import { Repl } from 'solid-repl';
-import { Link } from 'solid-app-router';
+import { Link, NavLink } from 'solid-app-router';
 import { For, Component, Show, createSignal, createEffect, onCleanup } from 'solid-js';
+
+import { Icon } from '@amoutonbrady/solid-heroicons';
+import { chevronDown, chevronRight } from '@amoutonbrady/solid-heroicons/solid';
 
 import Nav from '../components/Nav';
 import Header from '../components/Header';
@@ -13,46 +16,56 @@ const DirectoryMenu: Component<{ directory: TutorialDirectory; current: Tutorial
   const [showDirectory, setShowDirectory] = createSignal(false);
   const listener = (e: MouseEvent) => {
     setShowDirectory(false);
-  }
+  };
+
   createEffect(() => {
     if (showDirectory()) {
-      window.addEventListener("click", listener);
+      window.addEventListener('click', listener);
     } else {
-      window.removeEventListener("click", listener);
+      window.removeEventListener('click', listener);
     }
   });
+
   onCleanup(() => {
-    window.removeEventListener("click", listener);
-  })
+    window.removeEventListener('click', listener);
+  });
+
   return (
-    <div
-      class="relative"
-    >
-      <div class="box-border rounded-t" classList={{ 'shadow-lg': showDirectory() }}>
+    <div class="relative z-10">
+      <div class="box-border rounded-t border-b border-solid ">
         <button
-          class="w-full border-b border-solid pb-2 flex flex items-center focus:outline-none"
-          onClick={(e) => { e.stopPropagation(); setShowDirectory(!showDirectory()) }}
+          class="pb-2 flex flex items-center focus:outline-none space-x-1 group"
+          onClick={(e) => {
+            e.stopPropagation();
+            setShowDirectory(!showDirectory());
+          }}
         >
           <div class="flex-grow inline-flex flex-col items-baseline">
             <h3 class="text-xl text-solid">{props.current?.lessonName}</h3>
             <p class="block text-gray-500 text-md">{props.current?.description}</p>
           </div>
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
+
+          <Icon
+            path={chevronDown}
+            class="h-8 -mb-1 transform transition group-hover:translate-y-0.5 duration-300"
+            classList={{ 'translate-y-0.5': showDirectory() }}
+          />
         </button>
       </div>
+
       <Show when={showDirectory()}>
-        <ul classList={{ 'shadow-lg': showDirectory() }}
-          class="block shadow absolute bg-white w-full divide-y box-border rounded-b"
-        >
+        <ul class="block shadow absolute bg-white w-2/3 h-[40vh] overflow-auto shadow-lg divide-y box-border rounded-b">
           <For each={props.directory}>
             {(entry) => (
-              <li class="hover:bg-blue-100">
-                <Link href={`/tutorial/${entry.internalName}`}>
+              <li>
+                <NavLink
+                  activeClass="bg-blue-50"
+                  class="hover:bg-blue-100 py-2 px-1 block"
+                  href={`/tutorial/${entry.internalName}`}
+                >
                   <p class="text-sm font-medium text-gray-900">{entry.lessonName}</p>
                   <p class="text-sm text-gray-500">{entry.description}</p>
-                </Link>
+                </NavLink>
               </li>
             )}
           </For>
@@ -69,9 +82,9 @@ const Tutorial: Component<TutorialProps> = (props) => {
       <Header title="SolidJS Tutorial" />
 
       <Show when={!props.loading} fallback={<p>Loading...</p>}>
-        <div style={{ width: '95vw' }} class="my-10 mx-auto">
-          <div class="grid grid-cols-12 gap-8">
-            <div class="col-span-4">
+        <div class="my-10 container mx-auto">
+          <div class="grid grid-cols-12 gap-12">
+            <div class="col-span-5 h-[75vh] relative overflow-hidden">
               <div>
                 <DirectoryMenu
                   current={props.tutorialDirectoryEntry}
@@ -80,17 +93,18 @@ const Tutorial: Component<TutorialProps> = (props) => {
               </div>
 
               <Show when={props.markdown} fallback={<p>Loading...</p>}>
-                <Markdown children={props.markdown} />
+                <Markdown class="py-8 h-full overflow-auto">{props.markdown}</Markdown>
               </Show>
             </div>
 
-            <div class="col-span-8">
+            <div class="col-span-7">
               <Show when={props.js} fallback={<p>Loading...</p>}>
                 <Repl
                   title="Interactive Example"
-                  height="85vh"
+                  height="75vh"
                   data={`${location.origin}${props.js}`}
                   isInteractive
+                  layout="vertical"
                   class="rounded-lg col-span-6 overflow-hidden shadow-2xl"
                 />
               </Show>
