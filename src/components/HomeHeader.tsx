0b946fcb8a725fc9212bce426ca0f842daf0bfb6
Joe Pea <joe@trusktr.io>
2021-06-03 16:43:29 -0700

    starting point: initial random placement, color, depth, and parallax

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,11 +1,16 @@
-import { Component, createSignal } from 'solid-js';
+import { Component, createEffect, createSignal } from 'solid-js';
 
 import logo from '../assets/logo.svg';
 import wordmark from '../assets/wordmark.svg';
 import { LumeHeaderBackground } from './LumeHeaderBackground';
 
-const MainHeaderContent: Component = () => (
-  <section class="px-3 lg:px-12 container space-y-10 lg:pb-20 lg:pt-52 py-10">
+const MainHeaderContent: Component<{ offsetX: number; offsetY: number }> = (props) => (
+  <section
+    class="px-3 lg:px-12 container space-y-10 lg:pb-20 lg:pt-52 py-10"
+    style={`
+      transform: translate3d(${props.offsetX}px, ${props.offsetY}px, 0);
+    `}
+  >
     <div class="flex flex-col justify-center lg:justify-start lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4">
       <img class="w-28  lg:w-48" src={logo} alt="Solid logo" />
       <img class="w-32  lg:w-52" src={wordmark} alt="Solid wordmark" />
@@ -19,9 +24,32 @@ const MainHeaderContent: Component = () => (
 const HomeHeader: Component = () => {
   const [mouseX, setMouseX] = createSignal(0);
   const [mouseY, setMouseY] = createSignal(0);
+  const [header, setHeader] = createSignal<HTMLElement | undefined>();
+  const [headerSize, setHeaderSize] = createSignal<HeaderSize>({ x: 0, y: 0 });
+
+  createEffect(() => {
+    if (!header()) return;
+
+    const rect = header().getBoundingClientRect();
+    setHeaderSize({ x: rect.width, y: rect.height });
+
+    const observer = new ResizeObserver((changes) => {
+      for (const change of changes) {
+        setHeaderSize({
+          x: change.contentBoxSize[0].inlineSize,
+          y: change.contentBoxSize[0].blockSize,
+        });
+      }
+    });
+
+    observer.observe(header());
+
+    return () => observer.disconnect();
+  });
 
   return (
     <header
+      ref={setHeader}
       style="position: relative; overflow: hidden"
       class="mx-3 rounded-br-3xl rounded-bl-3xl bg-gradient-to-r from-solid-light via-solid-medium to-solid-default text-white"
       onpointermove={(event) => {
@@ -36,15 +64,23 @@ const HomeHeader: Component = () => {
 
       {/* Place the main header content on top of the LUME scene. */}
       <div style="position: absolute; width: 100%; height: 100%; z-index: 2">
-        <MainHeaderContent />
+        <MainHeaderContent
+          // TODO consolidate with duplicate math in LumeHeaderBackground
+          offsetX={(mouseX() - headerSize().x / 2) / 20}
+          offsetY={(mouseY() - headerSize().y / 2) / 20}
+        />
       </div>
 
       {/* Hack: This is a duplicate of the main content so that it gives the
         header its natural size (otherwise the position:absolute of the first one
         causes the header to ignore it for its sizing calculations). */}
-      <MainHeaderContent />
+      <div style="visibility: hidden;">
+        <MainHeaderContent offsetX={0} offsetY={0} />
+      </div>
     </header>
   );
 };
 
 export default HomeHeader;
+
+type HeaderSize = { x: number; y: number };
--- src/components/LumeHeaderBackground.tsx
+++ src/components/LumeHeaderBackground.tsx
@@ -1,57 +1,73 @@
-import { Component, createEffect } from 'solid-js';
+/* @jsxImportSource @lume/element */
 
-import type { JSX as LUMEJSX } from 'lume';
+import { Component, createEffect, createSignal, For } from 'solid-js';
+import { Box, Scene, useDefaultNames, Motor, THREE } from 'lume';
 
-// We shouldn't need this, but it works, for now.
-// LUME's JSX export already augments the re-export of Solid's JSX, so I'm not
-// yet sure why the types aren't picked up if we don't do this here manually.
-declare module 'solid-js' {
-  namespace JSX {
-    interface IntrinsicElements extends LUMEJSX.IntrinsicElements {}
-  }
+let global = globalThis as any;
+
+if (!global.LUME_HOT_MODULE_RELOAD) {
+  useDefaultNames();
+  global.LUME_HOT_MODULE_RELOAD = true;
 }
 
-declare const LUME: typeof import('lume');
-
-LUME.useDefaultNames();
-
-const { Scene } = LUME;
+const colors = ['#4f88c6', 'white'];
 
 export const LumeHeaderBackground: Component<{ mouseX: number; mouseY: number }> = (props) => {
-  let box;
+  const camX = () => {
+    props.mouseX;
+    if (!scene) return 0;
+    return (props.mouseX - scene.calculatedSize.x / 2) / 20;
+  };
 
-  const scene = (
-    <lume-scene webgl>
+  const camY = () => {
+    props.mouseY;
+    if (!scene) return 0;
+    return (props.mouseY - scene.calculatedSize.y / 2) / 20;
+  };
+
+  var scene = (
+    <lume-scene webgl background-color="#446b9e">
       <lume-point-light color="white" position="500 -500 500" align-point="0.5 0.5 0.5" />
       <lume-ambient-light color="white" intensity="0.4" />
 
-      <lume-perspective-camera
-        align-point="0.5 0.5 0.5"
-        active
-        position={`${props.mouseX} ${props.mouseY} 500`}
-      />
-
-      <lume-box
-        ref={box}
-        size="100 100 100"
-        align-point="0.5 0.5 0.5"
-        mount-point="0.5 0.5 0.5"
-        color="white"
-      ></lume-box>
+      <lume-node align-point="0.5 0.5 0.5" position={`${camX()} ${camY()} 0`} rotation="0 0 -10">
+        <For each={Array.from({ length: 100 })}>
+          {(item, index) => (
+            <lume-box
+              size="50 50 50"
+              align-point="0.5 0.5 0.5"
+              mount-point="0.5 0.5 0.5"
+              color={colors[Math.round(1 * Math.random())]}
+              cast-shadow="false"
+              receive-shadow="false"
+              position={`
+                ${2000 * Math.random() - 1000}
+                ${1000 * Math.random() - 500}
+                ${-1400 * Math.random()}
+              `}
+              rotation={`0 ${360 * Math.random()} 0`}
+            />
+          )}
+        </For>
+      </lume-node>
     </lume-scene>
-  );
-
-  // FIXME Why TypeScript?
-  const _scene = scene as any as InstanceType<typeof Scene>;
+  ) as Scene;
 
   createEffect(() => {
     const { mouseX, mouseY } = props;
-    const size = _scene.calculatedSize;
+    const size = scene.calculatedSize;
 
-    console.log('mouse pos:', mouseX, mouseY);
+    // console.log('mouse pos:', mouseX, mouseY);
   });
 
-  box.rotation = (x, y, z) => [x, ++y, z];
+  // Wait for scene to be upgraded to a LUME.Scene instance.
+  setTimeout(() => {
+    // TODO add attributes for <lume-scene> for fog.
+    scene.three.fog = new THREE.FogExp2(new THREE.Color('#446b9e').getHex(), 0.0015);
+    scene.needsUpdate();
+  });
+
+  // scene.three.fog = new THREE.FogExp2(new THREE.Color('#345887').getHex(), 0.0015);
 
   return scene;
 };
