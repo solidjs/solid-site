7f8d25746bb7febfccf96e2c68fafb44a8af2651
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-30 15:55:18 -0500

    perf: set safari10 to false
    This reduces another 1 KB.
    
    Vite sets this to `true`.
    https://github.com/vitejs/vite/blob/7cd8d7832e12e2facf7dfc163320a8798e19c6fd/packages/vite/src/node/plugins/terser.ts#L24
    
    After w.r.t previous commit
    
    ```
    Name                               KiloBytes
    ----                               ---------
    _commonjsHelpers.8402d862.js       0.501
    404.5047071d.js                    0.729
    compiler.d09e48a5.c835169f.js      2509.098
    Contributors.4c361c7d.js           5.185
    css.066cb8c2.js                    4.229
    css.worker.9b184ef9.74608b2e.js    808.640
    cssMode.abdb295c.js                234.384
    Docs.cb2e9dc6.js                   2.523
    editor.worker.b74d6c51.eefb4bc4.js 87.311
    Examples.88bb9e05.js               2.961
    formatter.ffdb3533.6cb3cb92.js     854.075
    Home.ba3436bf.js                   13.175
    html.22e4341f.js                   4.277
    htmlMode.eae5517b.js               104.218
    index.338a9fc6.js                  0.629
    index.72419203.js                  47.038
    index3.2eaa03ca.js                 1096.424
    javascript.e30d8968.js             0.823
    jsonMode.c7392c47.js               73.969
    Media.b2725649.js                  4.096
    monacoTabs.2760e97d.js             1.045
    ReplTab.3c5c19c3.js                0.568
    setupRepl.d1d78894.js              19.697
    toggleHighContrast.c90a1c2d.js     2230.846
    ts.worker.50b09be9.86ffe2fb.js     4368.305
    tsMode.fd313ed4.js                 25.333
    Tutorial.e4d0ba93.js               153.719
    typescript.a40860b9.js             4.792
    vendor.277e6d57.js                 47.342
    ---------------------
    Sum size is 12705.9296875
    ```

--- .terserrc.js
+++ .terserrc.js
@@ -2,6 +2,7 @@ import TerserAtomic from "terser-config-atomic"
 
 export default {
   ...TerserAtomic,
+  safari10: false, // set to true by Vite, so we should unset it again
   compress: {
     ...TerserAtomic.compress,
     ecma: 2020,
