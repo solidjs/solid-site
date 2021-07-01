5369835cb1a96fb4157244bcdaf422255038476e
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-01 15:53:14 -0500

    fix: set unsafe_comps to false
    It fixes the issue that causes the typescript linter to detect all the words as reserved. It increases the size by 3 KB.
    
    ```
    Name                               KiloBytes
    ----                               ---------
    _commonjsHelpers.8402d862.js       0.501
    404.54eeef4f.js                    0.729
    compiler.85657ce1.b226c1ec.js      2509.369
    Contributors.5fa30d5e.js           5.185
    css.066cb8c2.js                    4.229
    css.worker.33906889.fb3dbda2.js    808.713
    cssMode.303727e9.js                234.410
    Docs.41277864.js                   3.755
    editor.worker.3be60899.e9fe25ab.js 87.356
    Examples.b3e48c78.js               2.961
    formatter.9391ed7f.6ed166d3.js     854.169
    Home.885d99bc.js                   13.167
    html.9632bd85.js                   4.277
    htmlMode.c720328b.js               104.226
    index.7f16aa72.js                  47.382
    index.7f720a86.js                  0.820
    index3.0c9f5b3d.js                 1096.464
    javascript.2544f1d7.js             0.823
    jsonMode.bbcf0976.js               73.986
    Media.32b34459.js                  4.096
    monacoTabs.88ecf544.js             1.045
    ReplTab.1173c364.js                0.568
    setupRepl.07217307.js              19.686
    toggleHighContrast.07d696fd.js     2231.236
    ts.worker.a079b25f.1e5aed99.js     4368.563
    tsMode.2364052c.js                 25.333
    Tutorial.73108268.js               153.802
    typescript.f80a091d.js             4.792
    vendor.3a23317e.js                 47.348
    ---------------------
    Sum size is 12708.9912109375
    
    ```

--- .terserrc.js
+++ .terserrc.js
@@ -5,6 +5,7 @@ export default {
   safari10: false, // set to true by Vite, so we should unset it again
   compress: {
     ...TerserAtomic.compress,
-    ecma: 2020,
+    ecma: 2020, // we target es2020
+    unsafe_comps: false, // causes the typescript linter to detect all the words as reserved
   },
 }
