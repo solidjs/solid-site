c5c3f2361b200b45071f9d183b2040bce5fedded
David Di Biase <david@pilotinterative.ca>
2021-07-02 11:31:00 -0400

    Added robots and removed useless footer anchor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,7 @@ const Footer: Component = () => (
   <Portal mount={document.getElementById('footer')!}>
     <div class="py-10 mt-5 bg-solid-lightgray rounded-tr-3xl rounded-tl-3xl mx-3">
       <div class="px-7 md:px-0 py-10 lg:px-12 container flex flex-col lg:flex-row items-center space-y-10 lg:space-y-0 lg:space-x-20">
-        <a href="/media"></a>
         <img class="w-40" src={wordmark} alt="Solid logo" />
-
         <div class="text-sm max-w-5xl">
           <p>
             Solid is an open-source project supported by a team of public contributors. It's
@@ -36,6 +34,7 @@ const Footer: Component = () => (
             <a
               class="mx-4 hover:opacity-50 transition"
               target="_blank"
+              rel="noopener"
               href="https://www.cloudflare.com/"
             >
               <img class="w-32" src={cloudflare} alt="cloudflare" />
@@ -43,6 +42,7 @@ const Footer: Component = () => (
             <a
               class="mx-4 hover:opacity-50 transition"
               target="_blank"
+              rel="noopener"
               href="https://www.builder.io/"
             >
               <img class="w-24" src={builder} alt="builder.io" />
@@ -50,6 +50,7 @@ const Footer: Component = () => (
             <a
               class="mx-4 hover:opacity-50 transition"
               target="_blank"
+              rel="noopener"
               href="https://www.saucelabs.com/"
             >
               <img class="w-32" src={sauce} alt="SAUCELABS" />
