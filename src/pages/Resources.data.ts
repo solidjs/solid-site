31064d9d9022747bfc2a178d85e6d1ab60bae6ad
David Di Biase <david@pilotinterative.ca>
2021-08-06 17:35:34 -0400

    Remain

--- src/pages/Resources.data.ts
+++ src/pages/Resources.data.ts
@@ -1,7 +1,7 @@
 import articles from './Resources/Articles.data';
 import utilities from './Resources/Utilities.data';
 import videos from './Resources/Videos.data';
-import Podcasts from './Resources/Podcasts.data';
+import podcasts from './Resources/Podcasts.data';
 
 export const ResourceData = () => ({
   list: [...utilities, ...videos, ...articles, ...podcasts],
