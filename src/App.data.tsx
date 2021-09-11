dc051710448100f20b39d993535e5b4e9e03b16e
David Di Biase <david@pilotinterative.ca>
2021-09-11 17:51:50 -0400

    Added navigator.language default selection

--- src/App.data.tsx
+++ src/App.data.tsx
@@ -1,7 +1,17 @@
-import { lazy, createEffect, createResource } from 'solid-js';
+import { createEffect, createResource } from 'solid-js';
 import { RouteDataFunc } from 'solid-app-router';
 import createCookieStore from '@solid-primitives/cookies-store';
-import { createI18nContext, I18nContext, useI18n } from '@solid-primitives/i18n';
+import { createI18nContext } from '@solid-primitives/i18n';
+
+const langs = {
+  en: async () => (await import('../lang/en/en')).default(),
+  it: async () => (await import('../lang/it/it')).default(),
+  ja: async () => (await import('../lang/ja/ja')).default(),
+  fr: async () => (await import('../lang/fr/fr')).default(),
+  id: async () => (await import('../lang/id/id')).default(),
+  he: async () => (await import('../lang/he/he')).default(),
+  'zh-cn': async () => (await import('../lang/zh-cn/zh-cn')).default(),
+};
 
 type DataParams = {
   locale: string;
@@ -10,10 +20,14 @@ type DataParams = {
 
 export const AppData: RouteDataFunc = (props) => {
   const [settings, set] = createCookieStore<{ locale: string }>();
+  const browserLang = navigator.language.slice(0, 2);
   if (props.location.query.locale) {
     set('locale', props.location.query.locale);
+  } else if (langs.hasOwnProperty(browserLang)) {
+    set('locale', browserLang);
   }
   const i18n = createI18nContext({}, settings.locale || 'en');
+  const [, { add }] = i18n;
   const params = (): DataParams => {
     const locale = i18n[1].locale();
     let page = props.location.pathname.slice(1);
@@ -22,27 +36,10 @@ export const AppData: RouteDataFunc = (props) => {
     }
     return { locale, page };
   };
-  const [lang] = createResource(params, async ({ locale }) => {
-    switch (locale) {
-      case 'en':
-        return (await import('../lang/en/en')).default();
-      case 'it':
-        return (await import('../lang/it/it')).default();
-      case 'ja':
-        return (await import('../lang/ja/ja')).default();
-      case 'fr':
-        return (await import('../lang/fr/fr')).default();
-      case 'id':
-        return (await import('../lang/id/id')).default();
-      case 'he':
-        return (await import('../lang/he/he')).default();
-      case 'zh-cn':
-        return (await import('../lang/zh-cn/zh-cn')).default();
-    }
-  });
+  const [lang] = createResource(params, ({ locale }) => langs[locale]());
   createEffect(() => set('locale', i18n[1].locale()));
   createEffect(() => {
-    if (!lang.loading) i18n[1].add(i18n[1].locale(), lang());
+    if (!lang.loading) add(i18n[1].locale(), lang() as Record<string, any>);
   });
   return {
     get i18n() {
