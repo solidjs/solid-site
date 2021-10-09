1a2a493a5cfd29d7a1bf38f767661eed1da84f0c
David Di Biase <1168397+davedbase@users.noreply.github.com>
2021-10-09 17:03:03 -0400

    Cleaned up transitions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,14 +18,18 @@ const Header: Component<{ title?: string }> = (props) => {
         if (collapsed() != result) {
           setCollapsed(result);
         }
-      },
-    ),
+      }
+    )
   );
   return (
     <>
-      <Transition enterToClass="opacity-100 max-h-full" exitToClass="max-h-0 opacity-0">
-        <Show when={!collapsed()}>
-          <header class="mx-2 rounded-br-3xl rounded-bl-3xl bg-gradient-to-r from-solid-light via-solid-medium to-solid-default text-white transition duration-5000 overflow-hidden">
+      <Transition
+        enterClass="max-h-0 opacity-0"
+        enterActiveClass="opacity-100"
+        exitToClass="max-h-0 opacity-0"
+      >
+        <Show when={collapsed() === false}>
+          <header class="mx-2 rounded-br-3xl rounded-bl-3xl bg-gradient-to-r from-solid-light via-solid-medium to-solid-default text-white transition duration-500 overflow-hidden">
             <div class="md:bg-hero dark:from-bg-gray-700 bg-no-repeat bg-right rtl:bg-left px-10">
               <section class="px-3 lg:px-12 container space-y-10 lg:pb-20 lg:pt-52 py-10">
                 <div class="flex items-center space-y-4 lg:space-y-0 lg:space-x-4">
@@ -41,34 +45,44 @@ const Header: Component<{ title?: string }> = (props) => {
         </Show>
       </Transition>
       <Nav showLogo={collapsed()} />
-      <Transition enterClass="opacity-100 max-h-52" exitToClass="max-h-0 opacity-0">
-        <Show when={collapsed() && !location.pathname.includes('tutorial')}>
-          <header class="bg-gradient-to-r from-solid-light via-solid-medium to-solid-default text-white text-center md:text-left rtl:text-right transition duration-5000 overflow-hidden">
+      <Transition
+        enterClass="opacity-0 max-h-0"
+        enterActiveClass="opacity-100 max-h-52"
+        exitToClass="opacity-0 max-h-0"
+      >
+        <Show when={collapsed() === true && !location.pathname.includes('tutorial')}>
+          <header class="bg-gradient-to-r from-solid-light via-solid-medium to-solid-default text-white text-center md:text-left rtl:text-right transition duration-500 overflow-hidden">
             <div class="px-3 lg:px-12 container">
               <h1 class="py-8 text-3xl">
-                <Switch>
-                  <Match when={location.pathname.includes('/blog')}>
-                    {t('global.blog.title', {}, 'Blog')}
-                  </Match>
-                  <Match when={location.pathname.includes('/guide')}>
-                    {t('docs.title', {}, 'Guides')}
-                  </Match>
-                  <Match when={location.pathname.includes('/docs')}>
-                    {t('docs.title', {}, 'Guides')}
-                  </Match>
-                  <Match when={location.pathname.includes('/resources')}>
-                    {t('resources.title', {}, 'Guides')}
-                  </Match>
-                  <Match when={location.pathname.includes('/examples')}>
-                    {t('examples.title', {}, 'Guides')}
-                  </Match>
-                  <Match when={location.pathname.includes('/media')}>
-                    {t('media.title', {}, 'Guides')}
-                  </Match>
-                  <Match when={location.pathname.includes('/contributors')}>
-                    {t('contributors.title', {}, 'Team & Contributions')}
-                  </Match>
-                </Switch>
+                <Transition
+                  enterClass="ml-5 opacity-0"
+                  enterActiveClass="opacity-100"
+                  exitToClass="ml-5 opacity-0"
+                >
+                  <Switch>
+                    <Match when={location.pathname.includes('/blog')}>
+                      <span>{t('global.blog.title', {}, 'Blog')}</span>
+                    </Match>
+                    <Match when={location.pathname.includes('/guide')}>
+                      <span>{t('docs.title', {}, 'Guides')}</span>
+                    </Match>
+                    <Match when={location.pathname.includes('/docs')}>
+                      <span>{t('docs.title', {}, 'Guides')}</span>
+                    </Match>
+                    <Match when={location.pathname.includes('/resources')}>
+                      <span>{t('resources.title', {}, 'Guides')}</span>
+                    </Match>
+                    <Match when={location.pathname.includes('/examples')}>
+                      <span>{t('examples.title', {}, 'Guides')}</span>
+                    </Match>
+                    <Match when={location.pathname.includes('/media')}>
+                      <span>{t('media.title', {}, 'Guides')}</span>
+                    </Match>
+                    <Match when={location.pathname.includes('/contributors')}>
+                      <span>{t('contributors.title', {}, 'Team & Contributions')}</span>
+                    </Match>
+                  </Switch>
+                </Transition>
               </h1>
             </div>
           </header>
