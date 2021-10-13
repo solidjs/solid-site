6e3a6b1ef7a938a9276a44192e70de24ede5ab03
Caleb Taylor <aquaductape@gmail.com>
2021-10-13 12:42:37 -0700

    dismiss-docs-dropdown

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -11,6 +11,7 @@ import Footer from '../components/Footer';
 import { Section } from '../../scripts/types';
 import { Icon } from '@amoutonbrady/solid-heroicons';
 import { useI18n } from '@solid-primitives/i18n';
+import Dismiss from 'solid-dismiss';
 
 interface DocData {
   loading: boolean;
@@ -43,6 +44,7 @@ const Docs: Component = (props) => {
     }
     setCurrent(prev.slug);
   }, 75);
+  let menuButton!: HTMLButtonElement;
   // Upon loading finish bind observers
   createEffect(() => {
     if (!data.loading) {
@@ -67,95 +69,98 @@ const Docs: Component = (props) => {
             classList={{
               'rotate-90': toggleSections(),
             }}
-            onClick={() => setToggleSections(!toggleSections())}
+            ref={menuButton}
           >
             <Icon class="h-7 w-7" path={chevronRight} />
           </button>
-          <div class="col-span-4 lg:col-span-3 relative">
-            <div
-              class={
-                'py-5 h-5/6 w-5/6 rounded-r-lg rounded-br-lg overflow-auto z-20 p-10 shadow-2xl border-2 border-gray-100 dark:bg-solid-gray bg-white fixed top-14 duration-300 transform ' +
-                'max-w-md lg:border-0 lg:shadow-none lg:p-0 lg:flex-col lg:top-12 ' +
-                'lg:sticky lg:flex'
-              }
-              classList={{
-                '-left-full': !toggleSections(),
-                'left-0': toggleSections(),
-              }}
-              style={{ height: 'calc(100vh - 5rem)', top: '4rem' }}
-            >
-              <ul class="overflow-auto mt-5 flex dark:text-white flex-col flex-1">
-                <For each={data.doc.sections}>
-                  {(firstLevel: Section) =>
-                    firstLevel.children?.length ? (
-                      <li>
-                        <button
-                          type="button"
-                          class="text-left w-full dark:text-white text-solid-medium border-b hover:text-gray-400 transition flex flex-wrap content-center justify-between space-x-2 text-sm p-2 py-4"
-                          onClick={() => setSection(firstLevel.title, (prev) => !prev)}
-                        >
-                          <span
-                            class="flex-1"
+          <Dismiss menuButton={menuButton} open={toggleSections} setOpen={setToggleSections} show>
+            <div class="col-span-4 lg:col-span-3 relative">
+              <div
+                class={
+                  'py-5 h-5/6 w-5/6 rounded-r-lg rounded-br-lg overflow-auto z-20 p-10 shadow-2xl border-2 border-gray-100 dark:bg-solid-gray bg-white fixed top-14 duration-300 transform ' +
+                  'max-w-md lg:border-0 lg:shadow-none lg:p-0 lg:flex-col lg:top-12 ' +
+                  'lg:sticky lg:flex'
+                }
+                classList={{
+                  '-left-full': !toggleSections(),
+                  'left-0': toggleSections(),
+                }}
+                style={{ height: 'calc(100vh - 5rem)', top: '4rem' }}
+              >
+                <ul class="overflow-auto mt-5 flex dark:text-white flex-col flex-1">
+                  <For each={data.doc.sections}>
+                    {(firstLevel: Section) =>
+                      firstLevel.children?.length ? (
+                        <li>
+                          <button
+                            type="button"
+                            class="text-left w-full dark:text-white text-solid-medium border-b hover:text-gray-400 transition flex flex-wrap content-center justify-between space-x-2 text-sm p-2 py-4"
+                            onClick={() => setSection(firstLevel.title, (prev) => !prev)}
+                          >
+                            <span
+                              class="flex-1"
+                              classList={{
+                                'font-semibold': current() == firstLevel.slug,
+                              }}
+                            >
+                              {firstLevel.title}
+                            </span>
+                            <Icon
+                              class="opacity-50 h-5 w-7 transform transition origin-center"
+                              classList={{
+                                'rotate-180 opacity-100': !!section[firstLevel.title],
+                                hidden: !firstLevel.children!.length,
+                              }}
+                              path={chevronDown}
+                            />
+                          </button>
+                          <ul
+                            class="overflow-hidden transition"
                             classList={{
-                              'font-semibold': current() == firstLevel.slug,
+                              'h-0': section[firstLevel.title] !== true,
+                              'h-full': section[firstLevel.title],
                             }}
                           >
-                            {firstLevel.title}
-                          </span>
-                          <Icon
-                            class="opacity-50 h-5 w-7 transform transition origin-center"
+                            <For each={firstLevel.children!}>
+                              {(secondLevel) => (
+                                <li onClick={() => setToggleSections(false)}>
+                                  <a
+                                    class="block px-5 border-b border-gray-100 pb-3 text-sm my-4 break-words"
+                                    classList={{
+                                      'text-solid hover:text-solid-dark':
+                                        `#${secondLevel.slug}` === props.hash,
+                                      'hover:text-gray-400': `#${secondLevel.slug}` !== props.hash,
+                                    }}
+                                    href={`#${secondLevel.slug}`}
+                                    children={secondLevel.title}
+                                  />
+                                </li>
+                              )}
+                            </For>
+                          </ul>
+                        </li>
+                      ) : (
+                        <li>
+                          <a
+                            class="text-left w-full dark:text-white text-solid-medium border-b hover:text-gray-400 transition flex flex-wrap content-center justify-between space-x-2 text-sm p-2 py-4"
                             classList={{
-                              'rotate-180 opacity-100': !!section[firstLevel.title],
-                              hidden: !firstLevel.children!.length,
+                              'font-semibold': current() == firstLevel.slug,
+                              'text-solid hover:text-solid-dark':
+                                `#${firstLevel.slug}` === props.hash,
+                              'hover:text-gray-400': `#${firstLevel.slug}` !== props.hash,
                             }}
-                            path={chevronDown}
+                            href={`#${firstLevel.slug}`}
+                            children={firstLevel.title}
                           />
-                        </button>
-                        <ul
-                          class="overflow-hidden transition"
-                          classList={{
-                            'h-0': section[firstLevel.title] !== true,
-                            'h-full': section[firstLevel.title],
-                          }}
-                        >
-                          <For each={firstLevel.children!}>
-                            {(secondLevel) => (
-                              <li onClick={() => setToggleSections(false)}>
-                                <a
-                                  class="block px-5 border-b border-gray-100 pb-3 text-sm my-4 break-words"
-                                  classList={{
-                                    'text-solid hover:text-solid-dark':
-                                      `#${secondLevel.slug}` === props.hash,
-                                    'hover:text-gray-400': `#${secondLevel.slug}` !== props.hash,
-                                  }}
-                                  href={`#${secondLevel.slug}`}
-                                  children={secondLevel.title}
-                                />
-                              </li>
-                            )}
-                          </For>
-                        </ul>
-                      </li>
-                    ) : (
-                      <li>
-                        <a
-                          class="text-left w-full dark:text-white text-solid-medium border-b hover:text-gray-400 transition flex flex-wrap content-center justify-between space-x-2 text-sm p-2 py-4"
-                          classList={{
-                            'font-semibold': current() == firstLevel.slug,
-                            'text-solid hover:text-solid-dark':
-                              `#${firstLevel.slug}` === props.hash,
-                            'hover:text-gray-400': `#${firstLevel.slug}` !== props.hash,
-                          }}
-                          href={`#${firstLevel.slug}`}
-                          children={firstLevel.title}
-                        />
-                      </li>
-                    )
-                  }
-                </For>
-              </ul>
+                        </li>
+                      )
+                    }
+                  </For>
+                </ul>
+              </div>
             </div>
-          </div>
+          </Dismiss>
+
           <div class="col-span-8 lg:col-span-9 px-10 lg:px-0">
             <Switch fallback={'Failed to load markdown...'}>
               <Match when={data.loading}>Loading documentation...</Match>
