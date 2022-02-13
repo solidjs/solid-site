f8efb50366677348b7a6a38a05c96510e473f2d0
David Di Biase <ddibiase@dbase-mac.local>
2022-02-13 17:29:56 -0500

    Updated release time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,7 +65,7 @@ const Footer: Component = () => {
             <div class="flex justify-between">
               <p class="text-sm text-center text-gray-600 dark:text-gray-300">
                 {t('global.footer.updated', {
-                  date: '2022/02/13, 10:00am',
+                  date: '2022/02/13, 5:30pm',
                   version: '1.3.3',
                 })}
               </p>
