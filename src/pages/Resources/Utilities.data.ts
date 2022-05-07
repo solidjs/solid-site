fceff881273ac9a08e0b0f3015455bbe3c7fd607
Tom Byrer <tomByrer@gmail.com>
2022-05-07 11:24:57 -0500

    FIXME: @motionone/solid
    https://discord.com/channels/722131463138705510/861229287868858379/972521594368958495

--- src/pages/Resources/Utilities.data.ts
+++ src/pages/Resources/Utilities.data.ts
@@ -1515,6 +1515,19 @@ const utilities: Array<Resource> = [
     categories: [ResourceCategory.Plugins, ResourceCategory.UI],
     published_at: 1651891327000,
   },
+  {
+    link: 'https://motion.dev/solid/quick-start',
+    title: '@motionone/solid',
+    description:
+      'Offical Motion One adapter, a hardware accelerated animations library, adopted by the great @davidbase & @thetarnav who hopefully will edit this description & published_at sometime soon ;).',
+    author: 'davidbase & thetarnav',
+    author_url: 'https://github.com/davedbase',
+    keywords: ['motionone', 'motion', 'animation', 'official'],
+    official: false,
+    type: ResourceType.Package,
+    categories: [ResourceCategory.UI],
+    published_at: 1646088393000,
+  },
 ];
 
 export default utilities;
