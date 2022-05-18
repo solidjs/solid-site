f7d319b8741b2e11517c6cd7769e698bfd41dd1f
David Di Biase <ddibiase@macbook-pro.lan>
2022-05-18 15:16:03 -0400

    Adjusted deploy time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const Footer: Component = () => {
             <div class="flex justify-between">
               <p class="text-sm text-center text-gray-600 dark:text-gray-300">
                 {t('global.footer.updated', {
-                  date: '2022/05/16, 12:30pm',
+                  date: '2022/05/18, 3:30pm',
                   version: '1.4.1',
                 })}
               </p>
