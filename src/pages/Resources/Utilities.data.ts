c8475d5c3fb218f92cea982c47dbbd6ac4fe2e7a
miguelsalesvieira <miguel.sales.vieira@gmail.com>
2022-10-02 17:28:29 +0100

    Add solid-flow to ecosystem packages
    I would like to contribute to the ecosystem by adding solid-flow, which is a simple component that allows developers to easily build interactive node-based diagrams and editors with solidjs. You can check the documentation on the repo for more information as well as a live demo.

--- src/pages/Resources/Utilities.data.ts
+++ src/pages/Resources/Utilities.data.ts
@@ -1748,6 +1748,18 @@ const utilities: Array<Resource> = [
     type: PackageType.Package,
     categories: [ResourceCategory.Starters, ResourceCategory.BuildUtilities],
   },
+  {
+    link: 'https://github.com/miguelsalesvieira/solid-flow',
+    title: 'solid-flow',
+    description:
+      'A simple component for building interactive node-based diagrams and editors.',
+    author: 'Miguel Vieira',
+    author_url: 'https://github.com/miguelsalesvieira',
+    keywords: ['ui', 'drag', 'flow'],
+    official: false,
+    type: PackageType.Package,
+    categories: [ResourceCategory.UI],
+  },
 ];
 
 export default utilities;
