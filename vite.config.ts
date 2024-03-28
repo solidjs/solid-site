a9060a2a043a075289c291977012ced2f0417067
Brian-Pob <brianpoblete77777@gmail.com>
2024-03-28 03:18:55 -0400

    apply formatting

--- vite.config.ts
+++ vite.config.ts
@@ -5,32 +5,32 @@ import rehypeHighlight from 'rehype-highlight';
 import remarkGfm from 'remark-gfm';
 import pckg from './package.json' assert { type: 'json' };
 import { VitePWA } from 'vite-plugin-pwa';
-import pwaoptions from './pwaoptions'
+import pwaoptions from './pwaoptions';
 
 export default defineConfig({
-	define: {
-		__UPDATED_AT__: JSON.stringify(new Date().toLocaleString()),
-		__SOLID_VERSION__: JSON.stringify(pckg.dependencies["solid-js"]),
-	},
-	plugins: [
-		{
-			...mdx({
-				jsx: true,
-				jsxImportSource: "solid-js",
-				providerImportSource: "solid-mdx",
-				remarkPlugins: [remarkGfm],
-				rehypePlugins: [rehypeHighlight],
-			}),
-			enforce: "pre",
-		} as Plugin,
-		solid({ extensions: [".md", ".mdx"] }),
-		VitePWA(pwaoptions),
-	],
-	optimizeDeps: {
-		include: ["monaco-textmate", "onigasm", "monaco-editor-textmate"],
-		exclude: ["@solid.js/docs"],
-	},
-	build: {
-		target: "esnext",
-	},
+  define: {
+    __UPDATED_AT__: JSON.stringify(new Date().toLocaleString()),
+    __SOLID_VERSION__: JSON.stringify(pckg.dependencies['solid-js']),
+  },
+  plugins: [
+    {
+      ...mdx({
+        jsx: true,
+        jsxImportSource: 'solid-js',
+        providerImportSource: 'solid-mdx',
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeHighlight()],
+      }),
+      enforce: 'pre',
+    } as Plugin,
+    solid({ extensions: ['.md', '.mdx'] }),
+    VitePWA(pwaoptions),
+  ],
+  optimizeDeps: {
+    include: ['monaco-textmate', 'onigasm', 'monaco-editor-textmate'],
+    exclude: ['@solid.js/docs'],
+  },
+  build: {
+    target: 'esnext',
+  },
 });
